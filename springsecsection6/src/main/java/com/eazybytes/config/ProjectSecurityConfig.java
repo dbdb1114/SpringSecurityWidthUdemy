e78b512f41897bd7233bfcd15957fdb6e06af959
yujeonghyeon <dbdb1114@naver.com>
2023-07-31 17:53:43 +0900

    cors Configuration

--- springsecsection6/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecsection6/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.eazybytes.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,9 +15,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 
 import javax.sql.DataSource;
+
+import java.util.Collection;
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -27,17 +33,33 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrfConfigurer->csrfConfigurer.disable());
+
+        http.cors(httpSecurityCorsConfigurer -> {
+            httpSecurityCorsConfigurer.configurationSource(new CorsConfigurationSource() {
+
+                @Override
+                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }
+            });
+        });
+
         /**
          * 아래에 권한이 필요한 url을 입력해준다.
          * */
         http.authorizeHttpRequests((requests) -> {
-            requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards")
-                    .authenticated()
-                    .requestMatchers("/notices", "/contact","/register")
-                    .permitAll();
+            requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                    .requestMatchers("/notices", "/contact","/register").permitAll();
         });
-            http.formLogin(withDefaults());
-            http.httpBasic(withDefaults());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
         return http.build();
 
     }
